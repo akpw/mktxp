6533a20d47117971e7fbc4435c213b34cb79b5eb
Arseniy Kuznetsov <k.arseniy@gmail.com>
2021-02-07 20:48:02 +0100

    DS refactor, fixes/optimizations

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(pkg_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mktxp',
-    version='0.25',
+    version='0.26',
 
     url='https://github.com/akpw/mktxp',
 
