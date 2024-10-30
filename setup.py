0567d0517f368f4beaeb32fa39c678cf24ae2534
Arseniy Kuznetsov <k.arseniy@gmail.com>
2024-10-30 08:19:17 +0000

    version inc

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(pkg_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mktxp',
-    version='1.2.8',
+    version='1.2.9',
 
     url='https://github.com/akpw/mktxp',
 
