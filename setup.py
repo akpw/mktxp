8878990c89aecd92f23c43433dbf04915fa142b3
Arseniy Kuznetsov <k.arseniy@gmail.com>
2026-02-28 09:48:09 +0100

    dev version sync

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(pkg_dir, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='mktxp',
-    version='1.2.17',
+    version='1.2.18',
 
     url='https://github.com/akpw/mktxp',
 
