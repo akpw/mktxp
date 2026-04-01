66301bbbb42b04e1b6887b81ccb183782e77b92b
bor <me@bor691.ir>
2026-04-01 15:33:32 +0330

    fix: last handshake was not being parsed correctly

--- mktxp/collector/wireguard_collector.py
+++ mktxp/collector/wireguard_collector.py
@@ -1,5 +1,6 @@
 from mktxp.collector.base_collector import BaseCollector
 from mktxp.datasource.wireguard_ds import WireguardPeerTrafficMetricsDataSource
+from mktxp.utils.utils import parse_mkt_uptime
 
 
 class WireGuardPeerCollector(BaseCollector):
@@ -14,7 +15,8 @@ class WireGuardPeerCollector(BaseCollector):
         interface_traffic_labels = ['disabled', 'name', 'comment', 'rx', 'tx', 'interface', 'current_endpoint_address',
                                     'current_endpoint_port', 'allowed_address', 'last_handshake']
         interface_traffic_translation_table = {
-            'disabled': lambda value: '1' if value == 'true' else '0'
+            'disabled': lambda value: '1' if value == 'true' else '0',
+            'last_handshake': parse_mkt_uptime,
         }
 
         interface_traffic_records = WireguardPeerTrafficMetricsDataSource.metric_records(
@@ -55,10 +57,10 @@ class WireGuardPeerCollector(BaseCollector):
             labels
         )
 
-        yield BaseCollector.counter_collector(
+        yield BaseCollector.gauge_collector(
             'last_handshake',
             'Time since last handshake with peer',
             interface_traffic_records,
-            'last-handshake',
+            'last_handshake',
             labels
         )
